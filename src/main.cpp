e6a8cf684c32f20be961e1f5a00b3c3b3c9d22af
Satvik Prasad <satvikprasad@Satviks-MacBook-Pro.local>
2025-04-17 18:55:12 +1000

    Made LEDs time dependent instead of tick dependent.

--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,54 @@
 #include <Arduino.h>
 #include <Servo.h>
 
+const int LEDS[4] = {11, 5, 6, 3};
 
-Servo myservo;
-Servo myservo2;
-int x = 0;
-int leds[4] = {11,5,6,3};
-float y;
-int sensor_value_1;
-int sensor_value_2;
-int adjusted_val_1;
-int adjusted_val_2;
+Servo servos[2];
 
 void setup(){
-  Serial.begin(9600);
+    Serial.begin(9600);
 
-  myservo.attach(9);
-  myservo2.attach(10);
+    servos[0].attach(9);
+    servos[1].attach(10);
 
-  pinMode(11, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(3, OUTPUT);
+    pinMode(11, OUTPUT);
+    pinMode(5, OUTPUT);
+    pinMode(6, OUTPUT);
+    pinMode(3, OUTPUT);
 }
 
 
-void loop(){
-  sensor_value_1 = map(analogRead(A0), 0, 1023, 0, 1000);
-  sensor_value_2 = analogRead(A1);
+void loop(){    
+    int sensor_val_1 = map(analogRead(A0), 0, 1023, 0, 1000);
+    int sensor_val_2 = analogRead(A1);
 
-  if ((sensor_value_1%100)<10){
-    adjusted_val_1 = map(sensor_value_1 - (sensor_value_1%100), 0, 1000, 0, 180);
-  } else if ((sensor_value_1%100)>90){
-    adjusted_val_1 = map(sensor_value_1 + (100-(sensor_value_1%100)), 0, 1000, 0, 180); ;
-  }
-  // int adjusted_val_1 = map(sensor_value_1, 0, 1020, 0, 9)*20;
-  adjusted_val_2 = map(sensor_value_2, 0, 1023, -45, 45);
+    int adjusted_val_1, adjusted_val_2;
 
-  myservo.write(adjusted_val_1-adjusted_val_2);
-  myservo2.write(180-adjusted_val_1-adjusted_val_2);
+    // Fractional part of sensor val 1 (to nearest 100)
+    int frac_sensor_val_1 = sensor_val_1 % 100;
+    int floor_sensor_val_1 = sensor_val_1 - frac_sensor_val_1;
 
-  y = x%2000;
-  for(int i=0; i<4; i++){
-    analogWrite(leds[i], (sin((y/2000*2*M_PI)+(i*M_PI_2))+1)*127.5);
-  }
+    if (frac_sensor_val_1 < 10) {
+        adjusted_val_1 = map(floor_sensor_val_1, 0, 1000, 0, 180);
+    } else if ((sensor_val_1 % 100)>90){
+        adjusted_val_1 = map(100 + floor_sensor_val_1, 0, 1000, 0, 180);
+    }
 
-  Serial.print("Servo 1: ");Serial.print(adjusted_val_1-adjusted_val_2);
-  Serial.print(" Servo 2: ");Serial.println(180-adjusted_val_1-adjusted_val_2);
+    // int adjusted_val_1 = map(sensor_val_1, 0, 1020, 0, 9)*20;
+    adjusted_val_2 = map(sensor_val_2, 0, 1023, -45, 45);
 
+    servos[0].write(adjusted_val_1-adjusted_val_2);
+    servos[1].write(180-adjusted_val_1-adjusted_val_2);
 
+    for(int i = 0; i < 4; ++i) {
+        float n = 2.f*M_PI*(1.f/2000.f);
+        float phase_shift = i*M_PI_2;
 
-  x+=15;
-  delay(15);
+        analogWrite(LEDS[i], (sin(millis()*n+phase_shift)+1.f)*127.5f);
+    }
+
+    Serial.print("Servo 1: ");Serial.print(adjusted_val_1-adjusted_val_2);
+    Serial.print(" Servo 2: ");Serial.println(180-adjusted_val_1-adjusted_val_2);
+    
+    delay(15)
 }
